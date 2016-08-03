cbf7483ce58ff808d59f15511a397e7b126a841b
Miikka Koskinen <miikka.koskinen@iki.fi>
2016-08-03 11:19:15 +0300

    spec: Describe required properties for entity maps

--- src/ring/swagger/json_schema/spec.clj
+++ src/ring/swagger/json_schema/spec.clj
@@ -3,24 +3,20 @@
             [clojure.core.match :refer [match]]
             [ring.swagger.common :as c]))
 
-(defn map-spec?
-  [spec]
-  (let [desc (spec/describe spec)]
-    (when (seq? desc)
-      (= 'keys (first desc)))))
-
-(defn properties
-  [spec]
-  {:pre [(map-spec? spec)]}
-  (let [{:keys [req req-un opt opt-un]} (seq (rest (spec/describe spec)))
-        unk #(-> % name)]
-    (into {} (map (juxt name spec-object) (concat req req-un opt opt-un)))))
+(defn keys->swagger
+  [{:keys [req req-un opt opt-un]}]
+  (let [properties (into {} (map (juxt name spec-object)
+                                 (concat req req-un opt opt-un)))]
+    {:type "object"
+     :properties properties
+     :required (map name (concat req req-un))
+     :additionalProperties false}))
 
 (defn spec-object
   "Returns a JSON schema matching a spec."
   [spec]
   (match (if (symbol? spec) spec (spec/describe spec))
-    (['keys & _]       :seq) {:type "object" :properties (properties spec)}
+    (['keys & args]    :seq) (keys->swagger args)
     (['tuple & args]   :seq) {:type "array" :items (map spec-object args) :minItems (count args)}
     (['every type & _] :seq) {:type "array" :items (spec-object type)}
     (['cat & args]     :seq) {:type "array"}  ; the regex support makes this special!
